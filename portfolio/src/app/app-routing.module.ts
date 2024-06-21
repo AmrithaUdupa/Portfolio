fd2f8f05bfdb966f87a5e0734d7e9a68f5601120
AmrithaUdupa <udupaamritha54@gmail.com>
2024-06-21 16:27:01 +0530

    Created navbar and home page
    Added routing to navigate to navbar and created home page

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { LoginSignupComponent } from './pages/login-signup/login-signup.component';
+import { LayoutComponent } from './sharepage/layout/layout.component';
+import { authGuard } from './shared/auth.guard';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:LayoutComponent,canActivateChild:[authGuard],
+    children:[
+      {
+        path:'home',
+        component:HomeComponent,
+      },
+      {
+        path:'about',
+        component:AboutComponent,
+      },
+      {
+        path:'experience',
+        component:ExperienceComponent,
+      },
+      {
+        path:'contact',
+        component:ContactComponent,
+      },
+      {
+        path:'project',
+        component:ProjectComponent
+      }
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
