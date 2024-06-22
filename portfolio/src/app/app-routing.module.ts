f608c6a06a01c3490964b3b2b0a97fa2ce5ae874
AmrithaUdupa <udupaamritha54@gmail.com>
2024-06-22 19:45:21 +0530

    Updated logic for Authentication
    Those who have successfully logged in can access protected resources or pages.

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ import { ExperienceComponent } from './pages/experience/experience.component';
 import { ProjectComponent } from './pages/project/project.component';
 import { LoginSignupComponent } from './pages/login-signup/login-signup.component';
 import { LayoutComponent } from './sharepage/layout/layout.component';
-import { authGuard } from './shared/auth.guard';
+import { AuthGuard } from './shared/auth.guard';
 import { AboutComponent } from './pages/about/about.component';
 
 
 const routes: Routes = [
   {path:'',redirectTo:'/login-signup',pathMatch:'full'},
   {path:'login-signup',component:LoginSignupComponent},
-  {path:'',component:LayoutComponent,canActivateChild:[authGuard],
+  {path:'',component:LayoutComponent,canActivateChild:[AuthGuard],
     children:[
       {
         path:'home',
--- portfolio/src/app/shared/auth.guard.ts
+++ portfolio/src/app/shared/auth.guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateChildFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivateChild, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateChildFn = (childRoute, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivateChild {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivateChild(): boolean {
+    const isLoggedIn = this.auth.IsLoggedIn();
+    console.log('AuthGuard#canActivateChild called, isLoggedIn:', isLoggedIn);
+
+    if (this.auth.IsLoggedIn()) {
+      return true;
+    }
+
+    console.log('Redirecting to login-signup');
+    alert('Kindly Login first!!');
+    this.router.navigate(['./login-signup']);
+    return false;
+  }
+}
